2efa47581682f27d0cf377aa05455628c655d5a5
shailesh52 <sawantshailesh52@gmail.com>
2026-03-02 13:28:21 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomersPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
+      <h1 className="text-2xl font-bold">Customers</h1>
+    </main>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function InvoicesPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
+      <h1 className="text-2xl font-bold">Invoices</h1>
+    </main>
+  );
+}
